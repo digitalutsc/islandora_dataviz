78f6020d364ccdfcc13929a96cc19d02296e6db2
Natkeeran <natkeeran@gmail.com>
2020-07-30 17:06:50 +0000

    continuing dragomans demo viz

--- js/dataviz.js
+++ js/dataviz.js
@@ -1,6 +1,18 @@
 
 jQuery(window).load(function(){
 
-	alert("hello world");
+	var viewerDiv = jQuery(".islandora-binary-object-content").first();
+	var object_pid = Drupal.settings.islandora_dataviz.pid;
+
+	if (object_pid == "islandora:11") {
+	        viewerDiv.empty().append('<div width="1120" height="900" id="viz"></div>');
+		data = "https://raw.githubusercontent.com/digitalutsc/dragomans_visualizations/master/Network%20Diagram/viz1.json";
+		showNetworkDiagram(data);
+        } else if (object_pid == "islandora:12") {
+        	viewerDiv.empty().append('<div height="700" id="kingship-diagram-1" width="800"></div>');
+		data = "http://localhost:8000/sites/all/modules/islandora_dataviz/js/kinship1.js";
+		showKinshipDiagram(data);
+        }
+
 
 });
